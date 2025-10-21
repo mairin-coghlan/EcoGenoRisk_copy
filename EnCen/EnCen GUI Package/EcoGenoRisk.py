54908b4b0b9edf822298fc2e4e24c0844fa63d7d
mairin-coghlan <mairin.gk1@gmail.com>
2025-10-21 12:17:33 -0600

    Revert "Merge branch 'UCBoulder:main' into main"
    This reverts commit 5d511fb4c92948eb19f3c769ddfb282aebbeadd5, reversing
    changes made to a08c1095adeb2af314945adc49d08e655f1843ef.

--- EnCen/EnCen GUI Package/EcoGenoRisk.py
+++ EnCen/EnCen GUI Package/EcoGenoRisk.py
@@ -18,7 +18,11 @@ from matplotlib.colors import Normalize
 #_______________________________________________________________________________________
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2: 
+<<<<<<< HEAD
     st.image('EnCen/EnCen GUI Package/logo2.png', width=800)
+=======
+    st.image('/home/anna/Documents/EcoGenoRisk_Paper_Revisions/GUI_Output/logo2.png', width=800)
+>>>>>>> parent of 5d511fb (Merge branch 'UCBoulder:main' into main)
 # st.markdown("<h1 style='text-align: center;'>EcoGenoRisk</h1>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; font-size: 23px; color: #0F4A0F; '>Managing Synthetic Biology in the Environment</h1>", unsafe_allow_html=True)
 # st.markdown("<h1 style='text-align: center; font-size: 25px; '>Developed by John Docter (john.docter@colorado.edu), University of Colorado - Boulder>", unsafe_allow_html=True)
